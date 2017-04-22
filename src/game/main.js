08e7873c756c3b2d32b584b8a30a7ad8a58e2aa0
Sebastian Kopp <smkopp92@gmail.com>
2017-04-22 16:16:36 -0400

    two aliens

--- src/game/main.js
+++ src/game/main.js
@@ -1,16 +1,20 @@
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 var alien = new Alien(50,50);
+var alien2 = new Alien(150,150);
 
 function preload() {
   alien.preload();
+  alien2.preload();
   game.stage.backgroundColor = '#eee';
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
   alien.create();
+  alien2.create();
 }
 
 function update() {
   alien.update();
+  alien2.update();
 }
