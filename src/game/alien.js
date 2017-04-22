ab01491a16597e4e109e6fcc83476202eb1cad5e
Sam Backus <sbackus@patientslikeme.com>
2017-04-22 17:04:15 -0400

    move alienClass to alien

--- src/game/alien.js
+++ src/game/alien.js
@@ -1,36 +1,38 @@
-var alien = {
+class Alien {
+  constructor(starting_x, starting_y) {
+    this.starting_x = starting_x;
+    this.starting_y = starting_y;
+  }
 
-    preload: () => {
-      game.load.atlasJSONHash('alien', 'assets/p1_walk/p1_walk.png', 'assets/p1_walk/p1_walk.json');
-    },
+  preload() {
+    game.load.atlasJSONHash('alien', 'assets/p1_walk/p1_walk.png', 'assets/p1_walk/p1_walk.json');
+  }
 
-    create: () => {
-      sprite = game.add.sprite(game.world.width/2,0, 'alien');
+  create() {
+    this.sprite = game.add.sprite(this.starting_x, this.starting_y, 'alien');
+    game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+    this.sprite.body.collideWorldBounds = true;
+    this.sprite.animations.add('spin');
+    this.sprite.animations.play('spin', 20, true);
+    this.sprite.speed = 5;
+    this.sprite.anchor.setTo(0.5,0);
 
-      game.physics.enable(sprite, Phaser.Physics.ARCADE);
-      sprite.body.collideWorldBounds = true;
-      sprite.animations.add('spin');
-      sprite.animations.play('spin', 20, true);
-      sprite.speed = 5;
-      sprite.anchor.setTo(0.5,0);
+    this.sprite.inputEnabled = true;
+    this.sprite.events.onInputDown.add(this.reverse_direction, this);
+  }
 
-      sprite.inputEnabled = true;
-      sprite.events.onInputDown.add(alien.reverse_direction, this);
+  reverse_direction() {
+    this.sprite.speed *= -1;
+    this.sprite.scale.x *= -1;
+  }
 
-    },
-
-    reverse_direction: () => {
-      sprite.speed *= -1;
-      sprite.scale.x *= -1;
-    },
-
-    update: () => {
-      sprite.x += sprite.speed
-      if (sprite.x >= game.world.width - Math.abs(sprite.width * sprite.anchor.x)) {
-        alien.reverse_direction();
-      }
-      else if(sprite.x <= Math.abs(sprite.width * sprite.anchor.x)) {
-        alien.reverse_direction();
-      }
+  update() {
+    this.sprite.x += this.sprite.speed
+    if (this.sprite.x >= game.world.width - Math.abs(this.sprite.width * this.sprite.anchor.x)) {
+      this.reverse_direction();
     }
-};
+    else if(this.sprite.x <= Math.abs(this.sprite.width * this.sprite.anchor.x)) {
+      this.reverse_direction();
+    }
+  }
+}
