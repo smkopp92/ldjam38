0fd2d66dc18fb9c0b263478368d1a622bbb624e2
Sam Backus <sbackus@patientslikeme.com>
2017-04-22 19:13:45 -0400

    add bg to game

--- deploy/js/game.js
+++ deploy/js/game.js
@@ -63,6 +63,23 @@ class Alien {
   }
 }
 
+class Background {
+  constructor() {
+  }
+
+  preload() {
+    game.load.image('background', 'assets/Background.png');
+  }
+
+  create() {
+    this.sprite = game.add.sprite(0, 0, 'background');
+  }
+
+  update() {
+
+  }
+}
+
 class FloorSection {
   constructor(starting_x, starting_y, starting_width, starting_height, image) {
     this.starting_x = starting_x;
@@ -136,17 +153,21 @@ var alien2 = new Alien(150,150);
 var floorSection1 = new FloorSection(0,0,400,600, 'assets/Tiles/bg_castle.png');
 var floorSection2 = new FloorSection(400,0,400,600, 'assets/Tiles/box.png');
 var switch1 = new Switch(0,0,floorSection1, 'assets/Items/buttonRed.png', 'assets/Items/buttonRed_pressed.png');
+var background = new Background();
 
 function preload() {
+  background.preload();
   alien.preload();
   alien2.preload();
   floorSection1.preload();
   floorSection2.preload();
   switch1.preload();
+
   game.stage.backgroundColor = '#eee';
 }
 
 function create() {
+  background.create();
   game.physics.startSystem(Phaser.Physics.ARCADE);
   floorSection1.create();
   floorSection2.create();
--- src/game/z_main.js
+++ src/game/z_main.js
@@ -4,17 +4,21 @@ var alien2 = new Alien(150,150);
 var floorSection1 = new FloorSection(0,0,400,600, 'assets/Tiles/bg_castle.png');
 var floorSection2 = new FloorSection(400,0,400,600, 'assets/Tiles/box.png');
 var switch1 = new Switch(0,0,floorSection1, 'assets/Items/buttonRed.png', 'assets/Items/buttonRed_pressed.png');
+var background = new Background();
 
 function preload() {
+  background.preload();
   alien.preload();
   alien2.preload();
   floorSection1.preload();
   floorSection2.preload();
   switch1.preload();
+
   game.stage.backgroundColor = '#eee';
 }
 
 function create() {
+  background.create();
   game.physics.startSystem(Phaser.Physics.ARCADE);
   floorSection1.create();
   floorSection2.create();
