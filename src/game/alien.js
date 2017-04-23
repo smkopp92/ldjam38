566fa45b98a795dcc81121c09fdb302e83ea1407
Ezra Skolnik <ezraskolnik@gmail.com>
2017-04-22 23:38:40 -0400

    add directional movement on colored tiles

--- src/game/alien.js
+++ src/game/alien.js
@@ -14,26 +14,49 @@ class Alien {
     this.sprite.body.collideWorldBounds = true;
     this.sprite.animations.add('spin');
     this.sprite.animations.play('spin', 20, true);
+    this.sprite.headings = {x: 1, y: 1}
     this.sprite.speed = 5;
-    this.sprite.anchor.setTo(0.5,0);
+    this.sprite.anchor.setTo(0.5,0.5);
 
-    this.sprite.inputEnabled = true;
-    this.sprite.events.onInputDown.add(this.reverse_direction, this);
-  }
-
-  reverse_direction() {
-    this.sprite.speed *= -1;
-    this.sprite.scale.x *= -1;
+    // this.sprite.inputEnabled = true;
+    // this.sprite.events.onInputDown.add(this.reverse_direction, this);
   }
 
   update() {
-    this.sprite.x += this.sprite.speed
-    this.x = this.sprite.x
+    this.check_world_collision();
+    let axis = this.check_floor_color();
+    console.log(axis);
+    this.walk(axis);
+  }
+
+  walk(axis) {
+    this.sprite[axis] += this.sprite.speed * this.sprite.headings[axis]
+  }
+
+  check_floor_color() {
+    let newAxis = 'x';
+    floorSections.forEach ((tile) => {
+      if (tile.on && tile.sprite.overlap(this.sprite)) {
+        if (tile.color === "Green") {
+          newAxis = 'y';
+        }
+      }
+    });
+    return newAxis;
+  }
+
+  check_world_collision() {
     if (this.sprite.x >= game.world.width - Math.abs(this.sprite.width * this.sprite.anchor.x)) {
-      this.reverse_direction();
+      this.sprite.headings.x = -1;
     }
     else if(this.sprite.x <= Math.abs(this.sprite.width * this.sprite.anchor.x)) {
-      this.reverse_direction();
+      this.sprite.headings.x = 1;
+    }
+    if (this.sprite.y >= game.world.height - Math.abs(this.sprite.height * this.sprite.anchor.y)) {
+      this.sprite.headings.y = -1;
+    }
+    else if(this.sprite.y <= Math.abs(this.sprite.height * this.sprite.anchor.y)) {
+      this.sprite.headings.y = 1;
     }
   }
 }
--- src/game/z_main.js
+++ src/game/z_main.js
@@ -1,10 +1,13 @@
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
-var alien = new Alien(50, 50);
-var alien2 = new Alien(150, 150);
-var floorSection1 = new FloorSection(0, 0, 200, 600, 'Green');
-var floorSection2 = new FloorSection(400, 0, 200, 600, 'Blue');
-var switch1 = new Switch(0, 0, floorSection1, 'Green');
-var background = new Background();
+let game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+let alien = new Alien(50, 50);
+let alien2 = new Alien(150, 150);
+let floorSection1 = new FloorSection(0, 0, 200, 600, 'Green');
+let floorSection2 = new FloorSection(400, 0, 200, 600, 'Blue');
+let switch1 = new Switch(0, 0, floorSection1, 'Green');
+let background = new Background();
+let aliens;
+let floorSections;
+
 
 function preload() {
   background.preload();
@@ -25,6 +28,12 @@ function create() {
   switch1.create();
   alien.create();
   alien2.create();
+  // aliens = game.add.group();
+  // floorSections = game.add.group();
+  // aliens.add(alien);
+  // floorSections.addMultiple([floorSection1, floorSection2]);
+  aliens = [alien, alien2];
+  floorSections = [floorSection1, floorSection2];
 }
 
 function update() {
