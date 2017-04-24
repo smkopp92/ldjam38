169561c5870a01d324175c7d4a230c8dfeba050a
Suzi Curran <suzicurran@gmail.com>
2017-04-24 13:39:39 -0400

    goal sfx and adjusted goal position

--- src/game/level1.js
+++ src/game/level1.js
@@ -16,11 +16,11 @@ level1.prototype = {
     let wall2 = new Wall(150, 0, 70, 2000);
     let wall3 = new Wall(WORLDWIDTH-35, 0, 70, 2000);
     let wall4 = new Wall(150, WORLDHEIGHT-35, 4000, 70);
-    switch2 = new Switch('blueSwitch', 255, 212, 'assets/Items/buttonBlue_pressed.png', 'assets/Items/buttonBlue.png', false, true, false, toggle.bind(this, floorSection2), 90);
+    let switch1 = new Switch('blueButton', 255, 212, 'assets/Items/buttonBlue_pressed.png', 'assets/Items/buttonBlue.png', false, true, false, toggle.bind(this, floorSection2), 90);
     let playerButtonPanel = new ButtonPanel(
       () => {},
       () => {floorSection1.on = !floorSection1.on},
-      () => {floorSection2.on = !floorSection2.on},
+      () => {},
       () => {
         if (music) {
           music.stop();
@@ -29,15 +29,14 @@ level1.prototype = {
       }
     );
 
-    let goal1 = new Switch('goal1', 1050, 450, 'assets/Tiles/goal1.png', '', false, true, true, goToLevel2);
+    let goal1 = new Switch('goal1', 1100, 400, 'assets/Tiles/goal1.png', '', false, true, true, goToLevel2);
     game.load.audio('bgm', ['assets/Sound/Rainbow_Run.mp3']);
-    game.load.audio('goal_sfx', ['assets/Sound/goal_sfx.mp3']);
 
     aliens = [alien];
     floorSections = [floorSection1, floorSection2];
     walls = [wall1, wall2, wall3, wall4];
-    switches = [switch2];
-    gameObjects = aliens.concat(floorSections, walls, switches, goal1, playerButtonPanel)
+    switches = [goal1, switch1];
+    gameObjects = aliens.concat(floorSections, walls, switches, playerButtonPanel)
     preloadAll(gameObjects);
     game.stage.backgroundColor = '#eee';
 
